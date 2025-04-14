83e42a9ea78efc8073afeba395751b3b25e0324e
Enrique Rosa <enriquerosa777@gmail.com>
2025-04-14 09:36:24 +0200

    Variables.py

---
+++ variables.py
@@ -0,0 +1,16 @@
+#Definiendo una variable
+nombre = "Enrique "
+
+
+#conacatenar con +
+bienvenido = f"Hola " + " ¿Como estas?"
+
+# conacatenar con f strings 
+bienvenido = f"Hola {nombre} ¿Como estas?"
+
+
+
+#operadores de pertenencia (in / no in)
+print("Enrique" in bienvenido) #true
+
+print("Enrique" not in bienvenido) #false
