3ce62954072296037ece51ab67caef07cdb54327
Enrique Rosa <enriquerosa777@gmail.com>
2025-04-17 19:40:18 +0200

    Operadores Basicos.py

---
+++ 02_operadores.py
@@ -0,0 +1,28 @@
+### Operadores ###
+
+print(6 + 9)
+print(2 - 2)
+print(3 * 8)
+print(8 / 2)
+print(10 % 3)
+print(10 // 2)
+print(2 ** 2)
+
+
+print("Hola" + "Python")
+print("Hola " + str(5))
+print("Hola " * 5)
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+
+### Operadores Comparativos ### 
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4 )
+
