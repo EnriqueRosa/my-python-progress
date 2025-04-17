8c4220c017cbbd54cafe8dd5f1af5e129f489ba5
Enrique Rosa <enriquerosa777@gmail.com>
2025-04-17 11:24:05 +0200

    Variables.py

--- variables.py
+++ variables.py
@@ -1,16 +1,44 @@
-#Definiendo una variable
-nombre = "Enrique "
+# Variables
 
+my_string_variable =  "Hola Enrique"
+print("Hola Enrique")
 
-#conacatenar con +
-bienvenido = f"Hola " + " ¿Como estas?"
+my_int_variable = 5
+print(my_int_variable)
 
-# conacatenar con f strings 
-bienvenido = f"Hola {nombre} ¿Como estas?"
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable ))
 
+my_bool_variable = False
+print(my_bool_variable)
 
+# Concatenación de variables en un print
+print(my_string_variable, my_int_to_str_variable, my_bool_variable)
+print("Este es el valor de: , my_bool_variable")
 
-#operadores de pertenencia (in / no in)
-print("Enrique" in bienvenido) #true
+# Funciones del Sistema
+print(len(my_string_variable))
+ 
+# Variables en una sola línea. ¡Cuidado con abusar de esta sintaxis! 
+name, surname, alias, age = "Enrique", "Rosa", "Enric", 19
+print("Me llamo:",name, surname, ".Mi edad es:", age, "Y mi alias es:", alias)
 
-print("Enrique" not in bienvenido) #false
+# Inputs
+
+first_name = input('Cual es tu nombre: ')
+age = input('Cuantos años tienes? ')
+print(first_name)
+print(age)
+
+#Cambiamos su tipo
+
+name = 19
+age = "Enrique" 
+print(name)
+print(age)
+
+# Forzamos el tipo
+address: str = "Mi dirección"
+address = 34
+print(address)
